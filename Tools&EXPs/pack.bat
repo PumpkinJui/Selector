d3ac8519e340098e270bf787157a45ddc630f42d
PumpkinJui <76104432+PumpkinJui@users.noreply.github.com>
2022-10-12 11:00:07 +0800

    Update pack.bat

--- Tools&EXPs/pack.bat
+++ Tools&EXPs/pack.bat
@@ -1,3 +1,10 @@
 @echo off
-pyinstaller --version-file internal_file_version_info.txt -w -F "Selector Version 1.1 Internal Release.py" --clean
-pyinstaller --version-file public_file_version_info.txt -w -F "Selector Version 1.1 Public Release.py" --clean
+pyinstaller --version-file internal_file_version_info.txt -w -F "Selector Version 1.2 Internal Release.py"
+pyinstaller --version-file public_file_version_info.txt -w -F "Selector Version 1.2 Public Release.py"
+move dist\"Selector Version 1.2 Internal Release.exe"
+move dist\"Selector Version 1.2 Public Release.exe"
+rmdir /S /Q build
+rmdir /S /Q dist
+del "Selector Version 1.2 Internal Release.spec"
+del "Selector Version 1.2 Public Release.spec"
+pause
