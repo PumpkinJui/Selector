f1fce3fc70c5b40875b02abf55f0d9aad9dae472
PumpkinJui <76104432+PumpkinJui@users.noreply.github.com>
2022-10-15 23:13:12 +0800

    Create pack.bat

--- Tools&EXPs/pack.bat
+++ Tools&EXPs/pack.bat
@@ -1,10 +1,10 @@
 @echo off
-pyinstaller --version-file internal_file_version_info.txt -w -F "Selector 1.2 Internal.py"
-pyinstaller --version-file public_file_version_info.txt -w -F "Selector 1.2 Public.py"
-move dist\"Selector 1.2 Internal.exe"
-move dist\"Selector 1.2 Public.exe"
+pyinstaller --version-file internal_file_version_info.txt -w -F "Selector 1.3 Internal.py"
+pyinstaller --version-file public_file_version_info.txt -w -F "Selector 1.3 Public.py"
+move dist\"Selector 1.3 Internal.exe"
+move dist\"Selector 1.3 Public.exe"
 rmdir /S /Q build
 rmdir /S /Q dist
-del "Selector 1.2 Internal.spec"
-del "Selector 1.2 Public.spec"
+del "Selector 1.3 Internal.spec"
+del "Selector 1.3 Public.spec"
 pause
