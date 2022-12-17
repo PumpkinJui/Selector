7b34ae7728ab82e841cc5b54a49a6863367a2281
PumpkinJui <76104432+PumpkinJui@users.noreply.github.com>
2022-12-17 19:17:33 +0800

    Update pack.bat

--- Tools&EXPs/pack.bat
+++ Tools&EXPs/pack.bat
@@ -1,10 +1,14 @@
 @echo off
-pyinstaller --version-file internal_file_version_info.txt -w -F "Selector 1.3 Internal.py"
-pyinstaller --version-file public_file_version_info.txt -w -F "Selector 1.3 Public.py"
-move dist\"Selector 1.3 Internal.exe"
-move dist\"Selector 1.3 Public.exe"
+pyinstaller --version-file internal_file_version_info.txt -w --hidden-import pygame -F "Selector 1.4 Internal.py"
+pyinstaller --version-file public_file_version_info.txt -w --hidden-import pygame -F "Selector 1.4 Public.py"
+move dist\"Selector 1.4 Internal.exe" dist
+move dist\"Selector 1.4 Public.exe" dist
 rmdir /S /Q build
-rmdir /S /Q dist
-del "Selector 1.3 Internal.spec"
-del "Selector 1.3 Public.spec"
+del "Selector 1.4 Internal.spec"
+del "Selector 1.4 Public.spec"
+pyinstaller --version-file internal_file_version_info.txt -w --hidden-import pygame -D "Selector 1.4 Internal.py"
+pyinstaller --version-file public_file_version_info.txt -w --hidden-import pygame -D "Selector 1.4 Public.py"
+rmdir /S /Q build
+del "Selector 1.4 Internal.spec"
+del "Selector 1.4 Public.spec"
 pause
